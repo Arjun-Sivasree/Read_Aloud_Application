536e76380c4668aa8e8d364cb1524564d16a434e
arjunss <arjun.ss79@gmail.com>
2022-07-20 23:49:09 +0530

    removing unwanted files

--- Read_aloud_webapi/Controllers/Resource/AssignmentResource.cs
+++
@@ -1,9 +0,0 @@
-namespace Read_aloud_webapi.Controllers.Resource
-{
-    public class AssignmentResource
-    {
-        public int Id {get; set;}
-        public string? Description {get; set;}
-        public DateTime SubmissionDate {get; set;}
-    }
-}
